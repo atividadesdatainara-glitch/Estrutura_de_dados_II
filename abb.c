bcd85408530e49e638a922e2f0d5744683458d53
atividadesdatainara-glitch <atividadesdatainara@gmail.com>
2026-05-21 20:38:02 -0300

    Primeira parta do codigo

---
+++ abb.c
@@ -0,0 +1,34 @@
+//Etapa 1 — Cada nó da árvore precisa armazenar:
+//um valor;
+//ponteiro para filho da esquerda; 
+//ponteiro para filho da direita.
+
+//Em linguagem C:
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct No {
+    int valor;
+    struct No *esq;
+    struct No *dir;
+} No;
+
+
+//Etapa 1 — Cada nó da árvore precisa armazenar:
+//um valor;
+//ponteiro para filho da esquerda; 
+//ponteiro para filho da direita.
+
+//Em linguagem C:
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct No {
+    int valor;
+    struct No *esq;
+    struct No *dir;
+} No;
+
+
